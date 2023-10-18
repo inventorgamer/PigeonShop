3ae0bec7ba927975ad4cf22f6ea90cbd2968d088
Laste_5b0cp2n <ivan280104@gmail.com>
2023-10-18 10:36:43 +0800

    Change data list

--- app/src/main/java/com/invcorp/pigeonshop/MainActivity.kt
+++ app/src/main/java/com/invcorp/pigeonshop/MainActivity.kt
@@ -2,9 +2,8 @@ package com.invcorp.pigeonshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.invcorp.pigeonshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,20 +13,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val adapter = ShopProductsListAdapter()
-        adapter.addProductElement(ShopProductElement("Ананас Сатурн", 122, R.drawable.ananas))
-        adapter.addProductElement(ShopProductElement("Арбуз Лакомыч", 202, R.drawable.arbuz))
-        adapter.addProductElement(ShopProductElement("Яблоки Громыч-Паво", 35, R.drawable.apples))
-        adapter.addProductElement(ShopProductElement("Ананас Сатурн", 434, R.drawable.ananas))
-        adapter.addProductElement(ShopProductElement("Арбуз Лакомыч", 133, R.drawable.arbuz))
-        adapter.addProductElement(ShopProductElement("Яблоки Громыч-Паво", 23, R.drawable.apples))
+        adapter.addProductElement(ShopProductElement("Ананас", 251, R.drawable.ananas))
+        adapter.addProductElement(ShopProductElement("Арбуз", 202, R.drawable.arbuz))
+        adapter.addProductElement(ShopProductElement("Яблоки", 32, R.drawable.apples))
+        adapter.addProductElement(ShopProductElement("Ананас", 430, R.drawable.ananas))
+        adapter.addProductElement(ShopProductElement("Арбуз", 133, R.drawable.arbuz))
+        adapter.addProductElement(ShopProductElement("Яблоки", 22, R.drawable.apples))
         binding.rvShopList.adapter = adapter
         binding.rvShopList.layoutManager = GridLayoutManager(this,2)
 
 
-    }
-
-    private fun sandMessage(textPrint : String){
-        Toast.makeText(this, textPrint, Toast.LENGTH_LONG).show()
     }
 
 }
